01c185ee101d3d3454954d3738875d91837b698d
Wilson Gonzalez <wilson.i.gonzalez@unl.edu.ec>
2023-12-26 23:00:27 -0500

    Conexion de nodos

--- api/routes/nodo.js
+++ api/routes/nodo.js
@@ -42,4 +42,110 @@ router.delete("/:id", (req, res) => {
     .catch((err) => res.json({ message: err }));
 });
 
+router.post('/agregar-conexion', async (req, res) => {
+    const { nodo1, nodo2, distancia } = req.body;
+
+    try {
+        const nodoA = await Nodo.find({ nombre: nodo1 });
+        const nodoB = await Nodo.find({ nombre: nodo2 });
+
+        if (!nodoA || !nodoB) {
+            return res.status(404).send('Nodo no encontrado');
+        }
+
+        // Agrega la conexión al nodo A
+        nodoA.conexiones.push({
+            nodo: nodoB._id,
+            distancia: distancia
+        });
+
+        // Agrega la conexión al nodo B
+        nodoB.conexiones.push({
+            nodo: nodoA._id,
+            distancia: distancia
+        });
+
+        await nodoA.save();
+        await nodoB.save();
+
+        res.status(200).send('Conexión agregada con éxito');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.delete('/eliminar-conexion', async (req, res) => {
+    const { nodo1, nodo2 } = req.body;
+
+    try {
+        const nodoA = await Nodo.findOne({ nombre: nodo1 });
+        const nodoB = await Nodo.findOne({ nombre: nodo2 });
+
+        if (!nodoA || !nodoB) {
+            return res.status(404).send('Nodo no encontrado');
+        }
+
+        // Elimina la conexión del nodo A
+        nodoA.conexiones = nodoA.conexiones.filter(conexion => conexion.nodo.toString() !== nodoB._id.toString());
+
+        // Elimina la conexión del nodo B
+        nodoB.conexiones = nodoB.conexiones.filter(conexion => conexion.nodo.toString() !== nodoA._id.toString());
+
+        await nodoA.save();
+        await nodoB.save();
+
+        res.status(200).send('Conexión eliminada con éxito');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.put('/actualizar-conexion', async (req, res) => {
+    const { nodo1, nodo2, nuevaDistancia } = req.body;
+
+    try {
+        const nodoA = await Nodo.findOne({ nombre: nodo1 });
+        const nodoB = await Nodo.findOne({ nombre: nodo2 });
+
+        if (!nodoA || !nodoB) {
+            return res.status(404).send('Nodo no encontrado');
+        }
+
+        // Actualiza la distancia de la conexión en el nodo A
+        let conexionA = nodoA.conexiones.find(conexion => conexion.nodo.toString() === nodoB._id.toString());
+        if (conexionA) {
+            conexionA.distancia = nuevaDistancia;
+        }
+
+        // Actualiza la distancia de la conexión en el nodo B
+        let conexionB = nodoB.conexiones.find(conexion => conexion.nodo.toString() === nodoA._id.toString());
+        if (conexionB) {
+            conexionB.distancia = nuevaDistancia;
+        }
+
+        await nodoA.save();
+        await nodoB.save();
+
+        res.status(200).send('Distancia de conexión actualizada con éxito');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.get('/conexiones/:nodo', async (req, res) => {
+    const { nodo } = req.params;
+
+    try {
+        const nodoBuscado = await Nodo.findOne({ nombre: nodo }).populate('conexiones.nodo');
+
+        if (!nodoBuscado) {
+            return res.status(404).send('Nodo no encontrado');
+        }
+
+        res.status(200).json(nodoBuscado.conexiones);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 module.exports = router;
