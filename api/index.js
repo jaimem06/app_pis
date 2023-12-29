04108dff0d338a85364cd2c0c3e12ab6a49d0a70
Jaime Mendoza <jaimedavidmendoza1506@gmail.com>
2023-12-28 22:28:12 -0500

    Algoritmo Dijkstra Versión 2

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const nodoRoutes = require('./routes/nodo');
 const {crearGrafo} = require('./routes/grafo');
 const rutaMasCortaRouter = require('./routes/grafoRouter');
+const nodoRoutesV2 = require('./routes/nodocrudV2'); 
+const mejorRuta = require('./routes/mejor_ruta');
+const {createGrafoV2} = require('./utils/grafoV2');
 // 
 require('./db');
 require('./models/User');
@@ -19,6 +22,8 @@ app.use(authRoutes);
 
 app.use(express.json());
 app.use('/api/nodos',nodoRoutes);
+app.use('/api/nodosV2', nodoRoutesV2); // Añadido el uso del nuevo router
+app.use(mejorRuta); //Version 2
 app.use(rutaMasCortaRouter);
 //
 app.get('/crear-grafo', async (req, res) => {
@@ -30,11 +35,20 @@ app.get('/crear-grafo', async (req, res) => {
     }
 });
 
+app.get('/createGrafoV2', async (req, res) => {
+try {
+    const grafoV2 = await createGrafoV2();
+    res.json(grafoV2);
+} catch (error) {
+    res.status(500).send(error.toString());
+}
+});
 
 app.get('/', requireToken, (req, res) => {
     console.log(req.user);
     res.send(req.user);
 })
+
 
 app.listen(port, () => {
     console.log(`API funcionando en el puerto ${port}`);
---
+++ api/utils/grafoV2.js
@@ -0,0 +1,63 @@
+const nodoSchema = require('../models/nodo_modelV2.js');
+const dijkstra = require('./dijkstra.js');
+
+async function createGrafoV2 () {
+    // Obtén todos los nodos de la base de datos
+    const nodos = await nodoSchema.find();
+
+    // Crea un objeto para representar el grafo
+    const grafo = {};
+
+    // Para cada nodo en la base de datos
+    for (const nodo of nodos) {
+        // Usa el nombre del nodo como la clave en el objeto del grafo
+        const nombreNodo = nodo.properties.nombre;
+
+        // Crea una lista de adyacencia para este nodo
+        grafo[nombreNodo] = {};
+
+        // Comprueba si nodo.properties.conexiones es un array antes de intentar iterar sobre él
+        if (Array.isArray(nodo.properties.conexiones)) {
+            // Para cada conexión de este nodo
+            for (const conexion of nodo.properties.conexiones) {
+                // Busca el nodo al que esta conexión lleva
+                const nodoConectado = await nodoSchema.findById(conexion.nodo);
+
+                // Añade el nombre del nodo conectado a la lista de adyacencia
+                grafo[nombreNodo][nodoConectado.properties.nombre] = conexion.distancia;
+            }
+        } else {
+            console.error('nodo.properties.conexiones no es un array');
+        }
+    }
+
+    // Devuelve el grafo
+    return grafo;
+}
+
+async function rutaMasCorta(inicio, fin) {
+    try {
+        // Crea el grafo
+        const grafo = await createGrafoV2();
+        console.log('Grafo creado con éxito');
+
+        // Usa el algoritmo de Dijkstra para encontrar la ruta más corta
+        const ruta = dijkstra(grafo, inicio, fin);
+        console.log('Ruta calculada con éxito: ', ruta);
+
+        // Convierte los nombres de los nodos en la ruta a sus correspondientes coordenadas
+        const rutaCoordenadas = [];
+        for (const nombreNodo of ruta) {
+            const nodo = await nodoSchema.findOne({ 'properties.nombre': nombreNodo });
+            rutaCoordenadas.push(nodo.geometry.coordinates);
+        }
+
+        return rutaCoordenadas;
+    } catch (error) {
+        console.error('Error al calcular la ruta más corta: ', error);
+        throw error;
+    }
+}
+
+exports.createGrafoV2 = createGrafoV2;
+exports.rutaMasCorta = rutaMasCorta;
