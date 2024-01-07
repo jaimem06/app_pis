56cf7b6565986e919752891a85ca4eb8f3b99b52
Jaime Mendoza <jaimedavidmendoza1506@gmail.com>
2024-01-07 17:25:13 -0500

    Update estilos de pagina nodos y validaciones

---
+++ dashboard/src/styles/PageNodos.jsx
@@ -0,0 +1,82 @@
+
+//Estilos formulario
+export const formStyle = {
+    //Estilos flotantes
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '1000',
+    //////////////
+    display: 'flex',
+    flexDirection: 'column',
+    alightItems: 'center',
+    border: '1px solid black',
+    padding: '20px',
+    marginTop: '20px',
+    backgroundColor: '#003942',
+    width: '30%',
+    margin: '0 auto',
+    borderRadius: '10px',
+    fontSize: '15px'
+};
+
+//Estilos entrada de texto
+export const inputStyle = {
+    color: '#2A364E',
+    margin: '10px',
+    borderRadius: '5px',
+};
+
+export const buttonADD = {
+    backgroundColor: '#3E91E5',
+    borderRadius: '5px',
+    width: '45%',
+    color: '#2A364E',
+};
+
+export const titulosStyle = {
+    color: 'white',
+    textAlign: 'left',
+    fontSize: '16px',
+};
+
+export const tablaStyle = {
+    padding: '20px',
+    marginTop: '20px',
+    backgroundColor: '#2A364E',
+    width: '90%',
+    margin: '0 auto',
+    borderRadius: '10px',
+    fontSize: '15px'
+};
+
+export const filaStyle = {
+    border: '1px solid #001933"',
+    textAlign: 'center',
+};
+
+export const celdaStyle = {
+    border: '1px solid #001933',
+};
+
+export const deletebutton = {
+    backgroundColor: '#CE0000',
+    borderRadius: '4px',
+    width: '95%',
+    padding: '2px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '12px',
+};
+export const buttonCrearNodo = {
+    width: '18%',
+    backgroundColor: '#2A364E',
+    borderRadius: '8px',
+    border: '2px solid #3E91E5',
+    fontSize: '18px',
+    alightItems: 'left',
+    color: 'white',
+    margin: '20px'
+};
