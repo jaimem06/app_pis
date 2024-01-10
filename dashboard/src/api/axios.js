b3c9a33e6603ced381a56b43af2c51ca9a04382d
Jaime Mendoza <jaimedavidmendoza1506@gmail.com>
2024-01-10 18:36:51 -0500

    validaciones y restricciones para crud nodos

--- dashboard/src/api/axios.js
+++ dashboard/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
  const instance =axios.create({ 
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://api-fredunl.onrender.com',
+    //baseURL: 'http://localhost:3000',
 withCredentials: true,
  });
  
