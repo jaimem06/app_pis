e69ba75a58d1e530fba1aa374d13474f2aebe276
Jaime Mendoza <jaimedavidmendoza1506@gmail.com>
2024-01-13 01:07:32 -0500

    desplegandoWebDash

--- dashboard/src/api/axios.js
+++ dashboard/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
  const instance =axios.create({ 
-    //baseURL: 'https://api-fredunl.onrender.com',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://api-fredunl.onrender.com',
 withCredentials: true,
  });
  
