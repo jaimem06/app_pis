1a9f372664bbaa7c78e294a3cc6cd7bb812d4aa7
Wilson Gonzalez <wilson.i.gonzalez@unl.edu.ec>
2024-01-18 18:39:54 -0500

    infomacion de plan de emergencia en el mobil

--- mobile-app/pantallas/info.js
+++ mobile-app/pantallas/info.js
@@ -2,109 +2,80 @@ import React from 'react';
 import { Image } from 'react-native';
 import { Dimensions } from 'react-native';
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Linking } from 'react-native';
-
+import { useEffect, useState } from 'react';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 const Info = () => {
+  const [plan, setPlan] = useState(null);
+  useEffect(() => {
+    fetch('http://192.168.1.24:3000/planemergencia/read_plan/')
+      .then(response => response.json())
+      .then(data => setPlan(data[0]))
+
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <ScrollView> 
-    <View style={{backgroundColor:'white'}} >
-      <Image style = {{
-        width: windowWidth *1.0,
-        height: windowHeight *0.2,
-        alignSelf: "center",
-        
-      }} 
-      source={require('../imagenes/logo_unl.png')}
-      resizeMode='contain' />
-      <Image style = {{
-        width: windowWidth *0.5,
-        height: windowHeight *0.2,
-        alignSelf: "center",
-        
-      }}
-      source={require('../imagenes/logo.png')}
-      resizeMode='contain' />
-     <Image style = {{
-        width: windowWidth *0.5,
-        height: windowHeight *0.2,
-        alignSelf: "center",
-        
-      }}
-      source={require('../imagenes/unl.png')}
-      resizeMode='contain' />
-      <Text
-        style={{
-          fontSize: 25,
-          textAlign: "center",
-          marginBottom: "5%",
-          marginHorizontal : "5%", 
-        }}
-        
-      >      PLAN DE EMERGENCIA 
-      DE LA UNIVERSIDAD NACIONAL DE LOJA
-                  2022-2023
-      </Text>
-       <Text style={{
-          fontSize: 20,
-          textAlign: "justify",
-          justifyContent: "center",
-          marginHorizontal: "5%",
-          marginTop: "10%",
-          
-        }}
-      >El Plan de Emergencia Institucional de la Universidad Nacional de Loja tiene como objetivo principal cumplir con las disposiciones legales establecidas por la ley. Este plan se ha diseñado para hacer frente a diversas situaciones adversas, con el fin de salvaguardar la integridad física y la salud de todas las personas que se encuentren dentro de la institución, incluyendo personal docente, administrativo, trabajadores, estudiantes, visitantes y otros.</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        {plan && (
+          <View style={styles.planContainer}>
+             <Image source={{ uri: plan.imagen }} style={styles.image} />
+            <Text style={styles.title}>{plan.titulo}</Text>
+            <Text style={styles.text}>{plan.resumen}</Text>
+            <TouchableOpacity style={styles.link }
+              onPress={() => Linking.openURL(plan.link)}
+            >
+              <Text style={{ color: 'black', textAlign: 'center', fontSize: 20, }}>Plande Contingencia</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
 
-<Text style={{
-          fontSize: 20,
-          textAlign: "justify",
-          marginTop: "10%",
-          marginHorizontal: "5%"
-          
-        }}
-      >El documento también busca cumplir con el Artículo 14 del Reglamento de Higiene y Seguridad de la Universidad Nacional, que establece directrices para la prevención de amenazas naturales y riesgos antrópicos.</Text>
-
-<Text style={{
-          fontSize: 20,
-          textAlign: "justify",
-          marginTop: "10%",
-          marginHorizontal: "5%"
-        }}
-      > El Plan de Seguridad aborda tanto eventos naturales como sismos, como también desastres provocados por acciones humanas, tales como incendios, derrames de sustancias químicas, delincuencia, pandillaje, convulsiones sociales y accidentes de trabajo. Para cada uno de estos escenarios, se ha establecido un Plan de Contingencia que describe las acciones específicas a tomar.</Text>
-
-<Text style={{
-          fontSize: 20,
-          textAlign: "justify",
-          marginTop: "10%",
-          marginHorizontal: "5%"
-        }}
-      >Adicionalmente, se incluye un Plan de Evacuación que detalla el procedimiento para abandonar las edificaciones de manera rápida y efectiva. Se han identificado zonas seguras externas previamente establecidas para que todo el personal se desplace hacia ellas durante una emergencia.</Text>
-
-<Text style={{
-          fontSize: 20,
-          textAlign: "justify",
-          marginTop: "10%",
-          marginHorizontal: "5%"
-        }}
-      >La implementación de este plan cuenta con una organización bien estructurada, cuyos miembros están capacitados y entrenados adecuadamente. Cada miembro tiene responsabilidades y funciones específicas para actuar de manera efectiva en caso de una emergencia. Además, se cuentan con los medios de comunicación adecuados para coordinar y comunicar eficientemente durante situaciones de crisis.</Text>
-    
-<Text style={{
-          fontSize: 20,
-          textAlign: "justify",
-          marginTop: "10%",
-          marginHorizontal: "5%"
-        }}
-      >Para mas informacion:</Text>
-    
-    <TouchableOpacity style = {{ backgroundColor : 'skyblue', marginHorizontal : 50, marginTop : "10%",marginBottom:10 , padding:10, borderRadius : 5}}
-    onPress={() => Linking.openURL('https://www.unl.edu.ec/sites/default/files/servicios-tecnologicos/2022-05/PLAN%20EMERGENCIA%20UNL.pdf')} 
-  >
-    <Text style={{ color: 'black',textAlign :'center',fontSize: 20 ,  }}>Plande Contingencia</Text>
-  </TouchableOpacity> 
-    </View>
-    
     </ScrollView >
   );
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    textAlign: 'center',
+    marginTop: '10%',
+  
+  },
+  planContainer: {
+    width: 412,
+  },
+  title: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'justify',
+    marginHorizontal: 10,
+    marginTop: 10,
+    marginBottom: 10,
+   marginLeft: '7%',
+    marginRight: '15%',
+  },
+  link: {
+    backgroundColor: 'skyblue',
+    marginHorizontal: '7%',
+    marginTop: "10%",
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5, 
+    marginRight: '15%',
+  },
+});
 export default Info;
--- mobile-app/pantallas/logged_pantallas/info_contingencia.js
+++ mobile-app/pantallas/logged_pantallas/info_contingencia.js
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Image } from 'react-native';
+import { Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Linking } from 'react-native';
+import { useEffect, useState } from 'react';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const Info_Contingencia = () => {
+
+  const [plan, setPlan] = useState(null);
+  useEffect(() => {
+    fetch('http://192.168.1.24:3000/planemergencia/read_plan/')
+      .then(response => response.json())
+      .then(data => setPlan(data[0]))
+
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <View>
-      <Text>Aqui va informacion del plan de contingencia</Text>
-    </View>
+    <ScrollView style={{marginRight:'1%'}}>
+      <View style={styles.container}>
+        {plan && (
+          <View style={styles.planContainer}>
+             <Image source={{ uri: plan.imagen }} style={styles.image} />
+            <Text style={styles.title}>{plan.titulo}</Text>
+            <Text style={styles.text}>{plan.resumen}</Text>
+            <TouchableOpacity style={styles.link }
+              onPress={() => Linking.openURL(plan.link)}
+            >
+              <Text style={{ color: 'black', textAlign: 'center', fontSize: 20, }}>Plande Contingencia</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+
+    </ScrollView >
   )
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    textAlign: 'center',
+    marginTop: '10%',
+  
+  },
+  planContainer: {
+    width: 412,
+  },
+  title: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'justify',
+    marginHorizontal: 10,
+    marginTop: 10,
+    marginBottom: 10,
+   marginLeft: '7%',
+    marginRight: '15%',
+  },
+  link: {
+    backgroundColor: 'skyblue',
+    marginHorizontal: '7%',
+    marginTop: "10%",
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5, 
+    marginRight: '15%',
+  },
+});
 export default Info_Contingencia
