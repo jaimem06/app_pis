11868c5bfcb5fab499a93db19694ad48a9bbb8a2
Jaime Mendoza <jaimedavidmendoza1506@gmail.com>
2024-01-19 23:59:23 -0500

    mongo

--- api/db.js
+++ api/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+//Base de datos
+//Hola Mundo
+//Mongo DB
 mongoose.connect(process.env.mongo_URL).then(
     () => {
         console.log('Conexion exitosa a la base de datos');
