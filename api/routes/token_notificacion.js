24822ce6813ac68dbe913cf7edb7afa4caa0d66b
Jaime Mendoza <jaimedavidmendoza1506@gmail.com>
2024-01-28 14:47:45 -0500

    Correccion API

--- api/routes/token_notificacion.js
+++ api/routes/token_notificacion.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
 const PushToken = require('../models/notificacion_token');
 
-app.post('/guardar_tokenNotification', async (req, res) => {
+router.post('/guardar_tokenNotification', async (req, res) => {
     const { userId, token } = req.body;
 
-    // Validar que el token y el userId no sean nulos o vacíos
     if (!userId || !token) {
         return res.status(400).send({ success: false, message: 'Token o userId vacio' });
     }
@@ -13,11 +12,22 @@ app.post('/guardar_tokenNotification', async (req, res) => {
     // Comprobar que el token no se haya guardado previamente para el mismo usuario
     const existingToken = await PushToken.findOne({ userId });
     if (existingToken) {
-        return res.status(400).send({ success: false, message: 'El token ya ha sido guardado para este usuario' });
+        // Si el token existente es diferente al nuevo token, actualízalo
+        if (existingToken.token !== token) {
+            existingToken.token = token;
+            await existingToken.save();
+            return res.send({ success: true, message: 'Token actualizado' });
+        } else {
+            // Si el token existente es igual al nuevo token, no hagas nada
+            return res.send({ success: true, message: 'El token ya ha sido guardado para este usuario' });
+        }
     }
-    // Guardar el token en la base de datos
+
+    // Si no existe un token para este usuario, guárdalo
     const pushToken = new PushToken({ userId, token });
     await pushToken.save();
 
-    res.send({ success: true });
+    res.send({ success: true, message: 'Token guardado' });
 });
+
+module.exports = router;
