48359d513f2da5ef7edf4b83ec838760db8b2cba
Jaime Mendoza <jaimedavidmendoza1506@gmail.com>
2024-01-30 00:16:39 -0500

    Optimization Code

--- mobile-app/pantallas/brigadista.js
+++ mobile-app/pantallas/brigadista.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, FlatList } from 'react-native';
+import { Text, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 import { Dropdown } from 'react-native-element-dropdown';
 import { useNavigation } from '@react-navigation/native';
 import { handleNotificationResponse } from '../components/event_Notification';
 import APILinks from '../directionsAPI';
+import { responsabilidades, facultades, styles } from './styles/styles_brigadistas';
 
 const Brigadista = () => {
   const [selectedArea, setSelectedArea] = useState('');
@@ -30,17 +31,6 @@ const Brigadista = () => {
 
     fetchBrigadistas();
   }, [selectedArea]);
-
-  const facultades = [
-    "Administración Central",
-    "Bienestar Universitario",
-    "Educación a Distancia",
-    "Agropecuaria y de Recursos Nat Renovables",
-    "Energía, las Ind y los Recursos Nat No Renovables",
-    "Educación el Arte y la Comunicación",
-    "Jurídica, Social y Administrativa",
-    "Salud Humana"
-  ];
 
   return (
     <>
@@ -76,97 +66,24 @@ const Brigadista = () => {
                 ))}
                 <Card.Divider />
                 <Text style={styles.text}>Área/Piso donde se ubica: {brigadista.area}</Text>
-              </Card>
-              <Card.Title style={styles.cardTitle}>RESPONSABILIDADES</Card.Title>
-              {responsabilidades.map((responsabilidad, index) => (
-                <Card containerStyle={styles.innerCard} key={`responsabilidad-${index}`}>
-                  <Card.Title style={styles.taskTitle}>{responsabilidad.title}</Card.Title>
-                  {responsabilidad.tasks.map((task, taskIndex) => (
-                    <Text style={styles.text} key={`task-${taskIndex}`}>{task}</Text>
-                  ))}
                 </Card>
-              ))}
-            </Card>
+                <Card.Title style={styles.cardTitle}>RESPONSABILIDADES</Card.Title>
+                {responsabilidades.map((responsabilidad, index) => (
+                  <Card containerStyle={styles.innerCard} key={`responsabilidad-${index}`}>
+                    <Card.Title style={styles.taskTitle}>{responsabilidad.title}</Card.Title>
+                    {responsabilidad.tasks.map((task, taskIndex) => (
+                      <Text style={styles.text} key={`task-${taskIndex}`}>
+                        {'\u2022 '}{task}
+                      </Text>
+                    ))}
+                  </Card>
+                ))}
+                </Card>
           )}
         />
       )}
     </>
   );
 };
-
-const responsabilidades = [
-  {
-    title: "Antes de Evacuacion",
-    tasks: [
-      "Elaborar un croquis Interno",
-      "Determinar las Rutas de Evacuación, Salidas de emergencia.",
-      "Verificar que éstas no se encuentren bloqueadas, con llave ni obstaculizadas.",
-      "Prácticas diversas formas de rescate."
-    ]
-  },
-  {
-    title: "Durante Evacuacion",
-    tasks: [
-      "Cerrar las Llaves de agua, gas, cortar la energía eléctrica.",
-      "Alejar a los compañeros del área siniestrada",
-      "Conducir al Personal por la ruta de evacuación",
-      "Revisar que nadie quede en el área siniestrada."
-    ]
-  },
-  {
-    title: "Después de Evacuacion",
-    tasks: [
-      "Pasar lista de asistencia y comprobar que no falte nadie o identificar su paradero",
-      "Recorrer el inmueble ara establecer su estado y recomendar o no su utilización"
-    ]
-  }
-];
-
-const styles = StyleSheet.create({
-  title: {
-    color: '#B3DFE8',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-
-  titleP: {
-    color: '#2A364E',
-    fontSize: 23,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: '15%',
-  },
-
-  dropdown: {
-    width: '90%',
-    marginLeft: '5%',
-    marginTop: '10%',
-    backgroundColor: 'white',
-    marginBottom: '2%'
-  },
-  outerCard: {
-    backgroundColor: 'white',
-    borderRadius: 10
-  },
-  innerCard: {
-    backgroundColor: '#2A364E',
-    borderRadius: 10
-  },
-  cardTitle: {
-    color: '#007B8D',
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 'none'
-  },
-  taskTitle: {
-    color: '#B3DFE8',
-    fontSize: 14,
-    fontWeight: 'bold'
-  },
-  text: {
-    color: 'white'
-  }
-});
 
 export default Brigadista;
--- mobile-app/pantallas/logged_pantallas/info_brigadista.js
+++ mobile-app/pantallas/logged_pantallas/info_brigadista.js
@@ -1,63 +1,43 @@
-import APILinks from '../../directionsAPI';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { useEffect, useState } from 'react';
-import { FlatList, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 import { Dropdown } from 'react-native-element-dropdown';
+import { useNavigation } from '@react-navigation/native';
+import { handleNotificationResponse } from '../../components/event_Notification';
+import APILinks from '../../directionsAPI';
+import { responsabilidades, facultades, styles } from '../styles/styles_brigadistas';
 
-
-const Info_Brigadista = () => {
+const Brigadista = () => {
   const [selectedArea, setSelectedArea] = useState('');
   const [brigadista, setBrigadista] = useState([]);
-  useEffect(() => {
-    const fetchBrigadistas = async () => {
-      try {
-        const response = await fetch(APILinks.URL_Brigadista);
-        const data = await response.json();
-        setBrigadista(data); // Aquí cambiamos data[0] por data
-        console.log(selectedArea);
+  const navigation = useNavigation();
+  const [markers, setMarkers] = useState([]);
 
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchBrigadistas();
+  useEffect(() => {
+    const subscription = handleNotificationResponse(navigation, setMarkers);
+    return () => subscription.remove();
   }, []);
 
   useEffect(() => {
     const fetchBrigadistas = async () => {
       try {
-        const response = await fetch(APILinks.URL_Brigadista + `/area/${selectedArea}`);
+        const response = await fetch(APILinks.URL_Brigadista + (selectedArea ? `/area/${selectedArea}` : ''));
         const data = await response.json();
         setBrigadista(data);
       } catch (error) {
         console.error(error);
       }
     };
-    console.log(selectedArea);
-    if (selectedArea) {
-      fetchBrigadistas();
-    }
-  }, [selectedArea]);
 
-  const facultades = [
-    "Administración Central",
-    "Bienestar Universitario",
-    "Educación a Distancia",
-    "Agropecuaria y de Recursos Nat Renovables",
-    "Energía, las Ind y los Recursos Nat No Renovables",
-    "Educación el Arte y la Comunicación",
-    "Jurídica, Social y Administrativa",
-    "Salud Humana"
-  ];
+    fetchBrigadistas();
+  }, [selectedArea]);
 
   return (
     <>
-      <Text style={styles.title}>Brigadistas de rutas de evacuación</Text>
+      <Text style={styles.titleP}>Brigadistas de rutas de evacuación</Text>
       <Dropdown
-     style={{ width: 200 , marginLeft: '5%', marginTop: '10%',width:'90%',backgroundColor:'white',marginBottom:'2%'}}
-      data={facultades.map((facultad, index) => ({ label: facultad, value: index + 1 }))}
+        style={styles.dropdown}
+        data={facultades.map((facultad, index) => ({ label: facultad, value: index + 1 }))}
         search
         searchPlaceholder="Search"
         labelField="label"
@@ -67,97 +47,43 @@ const Info_Brigadista = () => {
         onChange={(item) => setSelectedArea(item.label)}
       />
       {Array.isArray(brigadista) && (
-        <>
-          <FlatList style={{ margin: '1%' }}
-            data={brigadista}
-            keyExtractor={(item) => item._id}
-            renderItem={({ item: brigadista }) => (
-              <Card>
-                <>
-                  <Card.Title>NOMBRES DE LOS
-                    MIEMBROS BRIGADA DE
-                    EVACUACIÓN</Card.Title>
-                  <Card>
-
-                    {brigadista.titular.map((titular, index) => (
-
-                      <Text key={`titular-${index}`}>
-                        Titular {titular.nroTitular} : {titular.nombresCompletos}
+        <FlatList
+          data={brigadista}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item: brigadista }) => (
+            <Card containerStyle={styles.outerCard}>
+              <Card.Title style={styles.cardTitle}>NOMBRES DE LOS MIEMBROS BRIGADA DE EVACUACIÓN</Card.Title>
+              <Card containerStyle={styles.innerCard}>
+                {brigadista.titular.map((titular, index) => (
+                  <Text style={styles.text} key={`titular-${index}`}>
+                    Titular {titular.nroTitular} : {titular.nombresCompletos}
+                  </Text>
+                ))}
+                {brigadista.reemplazo.map((reemplazo, index) => (
+                  <Text style={styles.text} key={`reemplazo-${index}`}>
+                    Reemplazo {reemplazo.nroReemplazo} : {reemplazo.nombresCompletos}
+                  </Text>
+                ))}
+                <Card.Divider />
+                <Text style={styles.text}>Área/Piso donde se ubica: {brigadista.area}</Text>
+                </Card>
+                <Card.Title style={styles.cardTitle}>RESPONSABILIDADES</Card.Title>
+                {responsabilidades.map((responsabilidad, index) => (
+                  <Card containerStyle={styles.innerCard} key={`responsabilidad-${index}`}>
+                    <Card.Title style={styles.taskTitle}>{responsabilidad.title}</Card.Title>
+                    {responsabilidad.tasks.map((task, taskIndex) => (
+                      <Text style={styles.text} key={`task-${taskIndex}`}>
+                        {'\u2022 '}{task}
                       </Text>
                     ))}
-                    {brigadista.reemplazo.map((reemplazo, index) => (
-                      <Text key={`reemplazo-${index}`}>
-                        Reemplazo {reemplazo.nroReemplazo} : {reemplazo.nombresCompletos}
-                      </Text>
-                    ))}
-                    <Card.Divider />
-                    <Text>Áre/Piso donde se ubica: {brigadista.area}</Text>
                   </Card>
-                  <Card.Title>RESPONSABILIDADES</Card.Title>
-                  <Card>
-
-                    <Card.Title>Antes de Evacuacion</Card.Title>
-                    <Text>Elaborar un croquis Interno</Text>
-                    <Text>Determinar las Rutas de Evacuación, Salidas de emergencia.</Text>
-                    <Text>Verificar que éstas no se encuentren bloqueadas, con llave ni obstaculizadas.</Text>
-                    <Text>Prácticas diversas formas de rescate.</Text>
-                  </Card>
-
-                  <Card>
-                    <Card.Title>Durante Evacuacion</Card.Title>
-                    <Text>Cerrar las Llaves de agua, gas, cortar la energía eléctrica.</Text>
-                    <Text>Alejar a los compañeros del área siniestrada</Text>
-                    <Text>Conducir al Personal por la ruta de evacuación</Text>
-                    <Text>Revisar que nadie quede en el área siniestrada.</Text>
-                  </Card>
-
-                  <Card>
-                    <Card.Title>Después de Evacuacion</Card.Title>
-                    <Text>Pasar lista de asistencia y comprobar que no falte nadie o identificar su paradero</Text>
-                    <Text>Recorrer el inmueble ara establecer su estado y recomendar o no su utilización</Text>
-                  </Card>
-                </>
-
-              </Card>
-            )}
-          />
-        </>
-
+                ))}
+                </Card>
+          )}
+        />
       )}
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    textAlign: 'center',
-    marginTop: '10%',
-
-  },
-  planContainer: {
-    width: 412,
-  },
-  title: {
-    color: 'black',
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: '5%',
-
-  },
-  responsable: {
-    backgroundColor: 'skyblue',
-    marginHorizontal: '7%',
-    marginTop: "10%",
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 5,
-    marginRight: '15%',
-    width: '85%',
-  },
-});
-
-
-export default Info_Brigadista
+export default Brigadista;
