7577ac998640b15e7d9939c18a39630e1d928b5f
Alyc3 <alyce.maldonado@unl.edu.ec>
2024-01-31 20:56:42 -0500

    Login
    Se cambió los estilos del Login

--- dashboard/src/pantallas/Pagina_login.jsx
+++ dashboard/src/pantallas/Pagina_login.jsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form'
 import { useAuth } from '../contexto/authcontext';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import fredUnlogo from '../../public/fred.png';
 
 
 function PaginaLogin() {
@@ -21,8 +22,10 @@ const onSubmit = handleSubmit((data) => {
         signin(data) })
         
     return (
+        <div className= 'divGeneral' style={{background: '#2a2e30'}}>
         <div className='flex h-screen items-center justify-center'>
-            <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+        <img src = '../../public/fred.png' style={{marginRight: '10%'}} />
+            <div className = 'bg-white max-w-md w-full p-10 rounded-md'>
             {
             signinErrors.map((error,i) => (
                 <div className="bg-red-500 p-2 text-white text-center m-2" key={i}>
@@ -30,7 +33,7 @@ const onSubmit = handleSubmit((data) => {
                 </div>  
             ))
             } 
-                <h1 className='text-2xl text-center text-white'>Login</h1>
+                <h1 className='text-2xl text-center text-black'>Login</h1>
                 <form onSubmit={onSubmit}>
                     <input type="text" {...register("email", { required: true })}
                         className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
@@ -46,9 +49,14 @@ const onSubmit = handleSubmit((data) => {
                     {
                         errors.password && <p className="text-red-500">Contraseña Requerida</p>
                     }
-                    <button type="submit">
+                    <div style= {{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <button type="submit" style = {{background: '#2a2e30',padding: '10px',borderRadius: '10px',marginTop: '10px',width: '35%'}}>
                         Login</button>
+                    </div>                   
+                    
                 </form>
+                
+            </div>
             </div>
         </div>
     )
