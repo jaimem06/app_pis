4be9c70fd760834231f55533bfc301def148a497
Alyc3 <alyce.maldonado@unl.edu.ec>
2024-02-05 09:59:12 -0500

    Update purtocom.js

--- api/routes/purtocom.js
+++ api/routes/purtocom.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
+const SerialPort = require('serialport').SerialPort;
+const Readline = require('@serialport/parser-readline').ReadlineParser;
+
 
 router.get('/com', (req, res) => {
-    const port = new SerialPort('COM3', {
+    const port = new SerialPort( {
+        path:'COM11',
         baudRate: 9600
     });
 
-    const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+    const parser = new Readline();
+    port.pipe(parser);
 
     parser.on('data', data => {
         console.log(data);
-        // Aquí puedes manipular los datos recibidos
-    });
+      });
 
     port.write('Hola, mundo!\n', error => {
         if (error) {
